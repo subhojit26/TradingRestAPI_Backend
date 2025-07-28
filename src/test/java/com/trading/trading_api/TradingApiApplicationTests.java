8aca2768ad87beb9424a0ea8a8d8077b7524a11c
Subhojit <ksubhojit20@gmail.com>
2025-07-28 21:21:57 +0530

    first commit

---
+++ src/test/java/com/trading/trading_api/TradingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trading.trading_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
